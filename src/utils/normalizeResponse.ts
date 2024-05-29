9435f46ed8043cf09e731c87fbacc06c1124ace6
DaniilSerga <daniil.serga@gmail.com>
2024-05-29 19:56:27 +0300

    refactor: remove logging

--- src/utils/normalizeResponse.ts
+++ src/utils/normalizeResponse.ts
@@ -3,6 +3,6 @@ import {ICourse} from "types/Course";
 export const normalizeResponse = (courses: ICourse[]) => {
     let tagsList = Array.from(new Set(courses.map(({tags}) => [...tags]).flat()));
     tagsList.unshift('Все категории');
-    console.log(tagsList);
+
     return {tagsList, courses};
 }
